ef1f325e7a3a4c763902670bdaba45e94992a40b
Your Name <imcarrosio@gmail.com>
2022-12-05 15:04:59 -0300

    v2

--- extract.py
+++ extract.py
@@ -7,26 +7,26 @@ import time
 path_data = 'data\scraped_apartments.json'
 path_links = 'data/link/links.json'
 
-index=[
-        'id',
-        'name',
-        'date',
-        'type_tx',
-        'zone',
-        'price',
-        'price_currency',
-        'aviable',
-        'expenses',
-        'currency_expenses',
-        'total_surface',
-        'total_covered',
-        'ambients',
-        'bedrooms',
-        'bathrooms',
-        'mascots',
-        'antiquity',
-        'orientation'
-    ]
+index = [
+    'id',
+    'name',
+    'date',
+    'type_tx',
+    'zone',
+    'price',
+    'price_currency',
+    'aviable',
+    'expenses',
+    'currency_expenses',
+    'total_surface',
+    'total_covered',
+    'ambients',
+    'bedrooms',
+    'bathrooms',
+    'mascots',
+    'antiquity',
+    'orientation'
+]
 
 try:
     scraped_apartments = pd.read_json(path_data)
@@ -34,33 +34,34 @@ except:
     scraped_apartments = pd.DataFrame([])
     pass
 
-#scraped_apartments = pd.DataFrame([], columns=index)
-
-
 links_df = pd.read_json(path_links)[0]
-
-driver = webdriver.Chrome(executable_path='chromedriver')
 
 errores = []
 list_objs = []
-already_used_id_list = []
 
-for i, link in enumerate(links_df):
-    
-    #now = time.time()
+
+already_used_id_list = scraped_apartments['id'].array
+
+only_new_links = []
+
+for x in links_df:
+    id = int(x.split('-')[1])
+    if not id in already_used_id_list:
+        only_new_links.append(x)
+
+print(len(only_new_links), len(links_df))
+
+
+driver = webdriver.Chrome(executable_path='chromedriver')
+
+for i, link in enumerate(only_new_links):
 
     id = int(link.split('-')[1])
-   
-    if not scraped_apartments.empty:
+    count_round = (len(already_used_id_list) + i)
+    result = (count_round / len(links_df)) * 100
+    print(round(result, 4), ' %') 
 
-        if id in scraped_apartments['id'].array:
-            print('already used')
-            continue
-
-    print((i / len(links_df)) * 100)
-    description_list = []
     driver.get(link)
-    
     # Delete Cookies Windows
     try:
         driver.find_element(
@@ -81,109 +82,97 @@ for i, link in enumerate(links_df):
     orientation = None
 
     try:
-        
-        table_catacteristics = driver.find_elements(By.CLASS_NAME, 'andes-table__row')
-        name = driver.find_element(By.XPATH, '/html/body/main/div/div[4]/div/div[2]/div[2]/div[1]/div/div[1]/div/p').text
-        type_tx = driver.find_element(By.XPATH, '/html/body/main/div/div[4]/div/div[1]/div[1]/div/div[1]/div/div[1]/span').text
+        driver.find_element(By.XPATH, '/html/body/main/div/div[2]/section/div[1]/div/div/div[2]/div[1]')
+        continue
+    except:
+
+        table_catacteristics = driver.find_elements(
+            By.CLASS_NAME, 'andes-table__row')
+        name = driver.find_element(
+            By.XPATH, '/html/body/main/div/div[4]/div/div[1]/div[1]/div/div[1]/div/div[2]/h1').text
+        type_tx = driver.find_element(
+            By.XPATH, '/html/body/main/div/div[4]/div/div[1]/div[1]/div/div[1]/div/div[1]/span').text
         #saller_name = driver.find_element(By.XPATH, '')
-        date = driver.find_element(By.XPATH, '/html/body/main/div/div[4]/div/div[1]/div[1]/div/div[1]/div/p').text
-        price = driver.find_element(By.XPATH, '/html/body/main/div/div[4]/div/div[1]/div[1]/div/div[2]/div/div/span/span[3]').text
-        price_currency = driver.find_element(By.XPATH, '/html/body/main/div/div[4]/div/div[1]/div[1]/div/div[2]/div/div/span/span[2]').text
-        zone_base = driver.find_elements(By.CLASS_NAME, 'andes-breadcrumb__link')
+        try:
+            date = driver.find_element(
+                By.XPATH, '/html/body/main/div/div[4]/div/div[1]/div[1]/div/div[1]/div/p').text
+        except:
+            date = driver.find_element(
+                By.XPATH, '/html/body/main/div/div[4]/div/div[1]/div[1]/div/div[1]/div/div[3]/p').text
+        price = driver.find_element(
+            By.XPATH, '/html/body/main/div/div[4]/div/div[1]/div[1]/div/div[2]/div/div/span/span[3]').text
+        price_currency = driver.find_element(
+            By.XPATH, '/html/body/main/div/div[4]/div/div[1]/div[1]/div/div[2]/div/div/span/span[2]').text
+        zone_base = driver.find_elements(
+            By.CLASS_NAME, 'andes-breadcrumb__link')
         for zone1 in zone_base:
             zone = zone1
 
-    except:
-        errores.append(link)
-        continue
 
-    for row in table_catacteristics:
+        for row in table_catacteristics:
+
+            title = row.find_element(By.TAG_NAME, 'th').text
+            data = row.find_element(By.TAG_NAME, 'td').text
+
+            if 'Ambientes' in title:
+                ambients = data
+
+            if 'Expensas' in title:
+                expenses = data.split(' ')[0]
+                expenses_currency = data.split(' ')[1]
+
+            if 'Antigüedad' in title:
+                antiquity = data.split(' ')[0]
+
+            if 'Dormitorios' in title:
+                bedrooms = data
+
+            if 'Orientación' in title:
+                orientation = data
+
+            if 'Superficie total' in title:
+                total_surface = data.split(' ')[0]
+
+            if 'Superficie cubierta' in title:
+                total_covered = data.split(' ')[0]
+
+            if 'mascotas' in title:
+                mascots = data
+
+            if 'Baños' in title:
+                bathrooms = data
+
+        obj = {
+            'id': id,
+            'name': name,
+            'date': date,
+            'type_tx': type_tx,
+            'zone': zone.text,
+            'price': price.replace('.', ''),
+            'price_currency': price_currency,
+            'aviable': available,
+            'expenses': expenses,
+            'currency_expenses': price_currency,
+            'total_surface': total_surface,
+            'total_covered': total_covered,
+            'ambients': ambients,
+            'bedrooms': bedrooms,
+            'bathrooms': bathrooms,
+            'mascots': mascots,
+            'antiquity': antiquity,
+            'orientation': orientation
+        }
+
+        if scraped_apartments.empty == False:
+            scraped_apartments = pd.read_json(path_data)
+
+        new_row = pd.DataFrame([obj], index=[len(scraped_apartments)])
         
-        title = row.find_element(By.TAG_NAME, 'th').text
-        data = row.find_element(By.TAG_NAME, 'td').text
-
-        if 'Ambientes' in title:
-            ambients = data
-
-        if 'Expensas' in title:
-            expenses = data.split(' ')[0]
-            expenses_currency = data.split(' ')[1]
-
-        if 'Antigüedad' in title:
-            antiquity = data.split(' ')[0]
-
-        if 'Dormitorios' in title:
-            bedrooms = data
-
-        if 'Orientación' in title:
-            orientation = data
-
-        if 'Superficie total' in title:
-            total_surface = data.split(' ')[0]
-
-        if 'Superficie cubierta' in title:
-            total_covered = data.split(' ')[0]
+        scraped_apartments = pd.concat([scraped_apartments, new_row])
         
-        if 'mascotas' in title:
-            mascots = data
-
-        if 'Baños' in title:
-            bathrooms = data
+        print(scraped_apartments)
         
-    obj = {
-        'id': id,
-        'name': name,
-        'date': date,
-        'type_tx': type_tx,
-        'zone': zone.text,
-        'price': price.replace('.',''),
-        'price_currency':price_currency,
-        'aviable': available,
-        'expenses': expenses,
-        'currency_expenses': price_currency,
-        'total_surface': total_surface,
-        'total_covered': total_covered,
-        'ambients': ambients,
-        'bedrooms': bedrooms,
-        'bathrooms': bathrooms,
-        'mascots': mascots,
-        'antiquity': antiquity,
-        'orientation': orientation
-    }
+        #scraped_apartments = scraped_apartments.append(
+        #    obj, ignore_index=True)
 
-
-    if scraped_apartments.empty == False:
-        scraped_apartments = pd.read_json(path_data)
-
-    scraped_apartments = scraped_apartments.append(obj, ignore_index = True)
-    scraped_apartments.to_json(path_data)
-    print(scraped_apartments)
-
-"""    if len(scraped_apartments) == 0:
-        new_apartament_scrapped = pd.DataFrame([obj])
-        new_apartament_scrapped.to_json(path_data)
-        scraped_apartments = pd.read_json(path_data)
-
-    else:
-        new_apartament_scrapped = pd.Series([obj])
-        frames = [scraped_apartments, new_apartament_scrapped.to_frame().T]
-        combined_scrapped_apartments = pd.concat(frames, ignore_index=True)
-        combined_scrapped_apartments.to_json(path_data)"""
-
-now = datetime.now()
-current_time = now.strftime("%H:%M:%S").replace(':', '_')
-
-
-
-
-    
-
-        
-    
-    
-
-        
-
-    
-
-
+        scraped_apartments.to_json(path_data)
